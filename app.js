4e8645322031fb5b1985ce8ed02e3f009b53bbfd
Davit Mnacakanyan <d.mnacakanyan@makebecool.com>
2022-08-27 08:08:59 +0400

    new updates

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/themes', themesRouter);
 app.use('/remix', randomRouter);
-app.use('/remix', async (req,res,next) => { res.redirect('/remix/62956f8717d82f2971e5c4fc?page=Home%20Page') })
+app.use('/remix', async (req,res,next) => { res.redirect('/remix/6306f8e7db2cbec8c440f780?page=Home%20Page') })
 app.use('/users', userThemesRouter);
 app.use('/view', viewRouter);
 app.use('/view/users', viewUserRouter);
