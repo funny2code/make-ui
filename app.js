6c6238c224608207eb8f1812e2730933e324a154
SmileCryptor <abc@gmail.com>
2022-10-26 03:55:32 +0800

    change default theme & fix text color of blog post theme

--- app.js
+++ app.js
@@ -1,80 +1,84 @@
-const createError = require('http-errors');
-const express = require('express');
-const session = require('express-session');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const cors = require('cors')
+const createError = require("http-errors");
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
 
-const indexRouter = require('./routes/index');
-const themesRouter = require('./routes/themes');
-const randomRouter = require('./routes/random');
-const randomViewRouter = require('./routes/randomview');
-const userThemesRouter = require('./routes/userthemes');
-const viewRouter = require('./routes/view');
-const viewUserRouter = require('./routes/viewuser');
-const getRouter = require('./routes/get');
-const saveRouter = require('./routes/save');
-const saveUserRouter = require('./routes/saveuser');
-const downloadRouter = require('./routes/download');
-const loginRouter = require('./routes/login');
-const logOutRouter = require('./routes/logout');
-const signupRouter = require('./routes/signup');
-const addThemeRouter = require('./routes/addtheme');
-const figmaRouter = require('./routes/figma');
+const indexRouter = require("./routes/index");
+const themesRouter = require("./routes/themes");
+const randomRouter = require("./routes/random");
+const randomViewRouter = require("./routes/randomview");
+const userThemesRouter = require("./routes/userthemes");
+const viewRouter = require("./routes/view");
+const viewUserRouter = require("./routes/viewuser");
+const getRouter = require("./routes/get");
+const saveRouter = require("./routes/save");
+const saveUserRouter = require("./routes/saveuser");
+const downloadRouter = require("./routes/download");
+const loginRouter = require("./routes/login");
+const logOutRouter = require("./routes/logout");
+const signupRouter = require("./routes/signup");
+const addThemeRouter = require("./routes/addtheme");
+const figmaRouter = require("./routes/figma");
 
 const app = express();
 
 const oneMonth = 31000 * 60 * 60 * 24;
-app.use(session({
+app.use(
+  session({
     secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
     saveUninitialized: true,
     cookie: { maxAge: oneMonth },
-    resave: false
-}));
-app.use(cors({origin: '*'}));
+    resave: false,
+  })
+);
+app.use(cors({ origin: "*" }));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use(logger('dev'));
-app.use(express.json({limit: '50mb'}));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(logger("dev"));
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/themes', themesRouter);
-app.use('/remix', randomRouter);
-app.use('/remix', async (req,res) => { res.redirect('/remix/6306f8e7db2cbec8c440f780?page=index') });
-app.use('/remixview', randomViewRouter);
-app.use('/users', userThemesRouter);
-app.use('/view', viewRouter);
-app.use('/view/users', viewUserRouter);
-app.use('/get', getRouter);
-app.use('/save', saveRouter);
-app.use('/save/users', saveUserRouter);
-app.use('/login', loginRouter);
-app.use('/logout', logOutRouter);
-app.use('/signup', signupRouter);
-app.use('/download', downloadRouter);
-app.use('/add', addThemeRouter);
-app.use('/figma', figmaRouter);
+app.use("/", indexRouter);
+app.use("/themes", themesRouter);
+app.use("/remix", randomRouter);
+app.use("/remix", async (req, res) => {
+  res.redirect("/remix/6306f8e7db2cbec8c440f780?page=index");
+});
+app.use("/remixview", randomViewRouter);
+app.use("/users", userThemesRouter);
+app.use("/view", viewRouter);
+app.use("/view/users", viewUserRouter);
+app.use("/get", getRouter);
+app.use("/save", saveRouter);
+app.use("/save/users", saveUserRouter);
+app.use("/login", loginRouter);
+app.use("/logout", logOutRouter);
+app.use("/signup", signupRouter);
+app.use("/download", downloadRouter);
+app.use("/add", addThemeRouter);
+app.use("/figma", figmaRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/random.js
+++ routes/random.js
@@ -1,120 +1,134 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const make = require('../contents/make');
-const modelThemes = require('../models/themes');
+const make = require("../contents/make");
+const modelThemes = require("../models/themes");
 // const modelSections = require('../models/sections');
-const path = require('path');
-const fs = require('fs').promises;
-const fonts = require('../contents/fonts');
-
-
-
+const path = require("path");
+const fs = require("fs").promises;
+const fonts = require("../contents/fonts");
 
 /* GET Theme Settings and Sections. */
-router.get('/:id', async (req, res, next) => {
-
-  const {id} = req.params;
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
   const page_handle = req.query.page;
 
-  if(!id || !page_handle) return next();
+  if (!id || !page_handle) return next();
 
-  try{
+  try {
     const theme = await modelThemes.findById(id).exec();
-    if(!theme) return next();
-    
-    const settingsFile = await fs.readFile(path.join(__dirname, `../themes/${id}/config/settings_data.json`), 'utf-8');
-    if(!settingsFile) return next();
+    if (!theme) return next();
+
+    const settingsFile = await fs.readFile(
+      path.join(__dirname, `../themes/${id}/config/settings_data.json`),
+      "utf-8"
+    );
+    if (!settingsFile) return next();
     const settings = JSON.parse(settingsFile);
-    const getPage = theme?.pages?.filter(page => page.handle === page_handle);
-    if(!getPage?.length) return next();
-    const sectionsFile = await fs.readFile(path.join(__dirname, `../themes/${id}/${getPage[0]?.template_name}.json`), 'utf-8');
-    if(!sectionsFile) return next();
+    console.log("settings:", settings);
+    const getPage = theme?.pages?.filter((page) => page.handle === page_handle);
+    if (!getPage?.length) return next();
+    const sectionsFile = await fs.readFile(
+      path.join(__dirname, `../themes/${id}/${getPage[0]?.template_name}.json`),
+      "utf-8"
+    );
+    if (!sectionsFile) return next();
     const parseSections = JSON.parse(sectionsFile);
 
-    res.render('random', {
+    res.render("random", {
       user: req?.session?.user || null,
-      isAdmin: req?.session?.user?.isAdmin || null, 
-      make: make, 
+      isAdmin: req?.session?.user?.isAdmin || null,
+      make: make,
       id: id,
       page_handle: page_handle,
       fonts: fonts,
       settingsSchema: theme?.settings_schema || null,
-      settings: settings?.current || null, 
+      settings: settings?.current || null,
       sectionsSchema: theme?.sections_schema || null,
       sections: parseSections,
-      pages: theme?.pages || null
+      pages: theme?.pages || null,
     });
-
-  } catch (err){
+  } catch (err) {
     return next(err);
   }
-
 });
 
 module.exports = router;
 
 /* POST Theme Settings and Sections. */
-router.post('/:id', async (req, res, next) => {
-
-  const {id} = req.params;
-  const {settings_data, templates} = req.body;
+router.post("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const { settings_data, templates } = req.body;
   const page_handle = req.query.page;
 
-  if(!id || !page_handle) return next();
+  if (!id || !page_handle) return next();
 
-  try{
+  try {
     const theme = await modelThemes.findById(id).exec();
-    if(!theme) return next();
-    
-    const settingsFile = await fs.readFile(path.join(__dirname, `../themes/${id}/config/settings_data.json`), 'utf-8');
-    if(!settingsFile) return next();
+    if (!theme) return next();
+
+    const settingsFile = await fs.readFile(
+      path.join(__dirname, `../themes/${id}/config/settings_data.json`),
+      "utf-8"
+    );
+    if (!settingsFile) return next();
     const settings = JSON.parse(settingsFile);
 
-    if(settings_data?.current && Object.keys(settings_data?.current).length > 1){
+    if (
+      settings_data?.current &&
+      Object.keys(settings_data?.current).length > 1
+    ) {
       Object.entries(settings_data?.current).forEach(([key, val]) => {
-        if(key && typeof val !== 'object'){
+        if (key && typeof val !== "object") {
           settings.current[key] = val;
         }
       });
     }
 
-    if(settings_data?.current?.sections && Object.keys(settings_data?.current?.sections).length > 0){
+    if (
+      settings_data?.current?.sections &&
+      Object.keys(settings_data?.current?.sections).length > 0
+    ) {
       Object.entries(settings_data?.current?.sections).forEach(([key, val]) => {
-          settings.current.sections[key] = val; 
+        settings.current.sections[key] = val;
       });
     }
 
-
-    const getPage = theme?.pages?.filter(page => page.handle === page_handle);
-    if(!getPage?.length) return next();
-    const sectionsFile = await fs.readFile(path.join(__dirname, `../themes/${id}/${getPage[0]?.template_name}.json`), 'utf-8');
-    if(!sectionsFile) return next();
+    const getPage = theme?.pages?.filter((page) => page.handle === page_handle);
+    if (!getPage?.length) return next();
+    const sectionsFile = await fs.readFile(
+      path.join(__dirname, `../themes/${id}/${getPage[0]?.template_name}.json`),
+      "utf-8"
+    );
+    if (!sectionsFile) return next();
     const parseSections = JSON.parse(sectionsFile);
 
-    if(Object.keys(templates).length > 0 && templates[page_handle] && templates[page_handle].sections && Object.keys(templates[page_handle].sections).length > 0){
+    if (
+      Object.keys(templates).length > 0 &&
+      templates[page_handle] &&
+      templates[page_handle].sections &&
+      Object.keys(templates[page_handle].sections).length > 0
+    ) {
       Object.entries(templates[page_handle].sections).forEach(([key, val]) => {
-          parseSections.sections[key] = val; 
+        parseSections.sections[key] = val;
       });
     }
 
-    res.render('random', {
+    res.render("random", {
       user: req?.session?.user || null,
-      isAdmin: req?.session?.user?.isAdmin || null, 
-      make: make, 
+      isAdmin: req?.session?.user?.isAdmin || null,
+      make: make,
       id: id,
       page_handle: page_handle,
       fonts: fonts,
       settingsSchema: theme?.settings_schema || null,
-      settings: settings?.current || null, 
+      settings: settings?.current || null,
       sectionsSchema: theme?.sections_schema || null,
       sections: parseSections,
-      pages: theme?.pages || null
+      pages: theme?.pages || null,
     });
-
-  } catch (err){
+  } catch (err) {
     return next(err);
   }
-
 });
 
 module.exports = router;
