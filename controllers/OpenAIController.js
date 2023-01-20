6e3deb87747efdc261f72cc131cd04d56464e66c
love1119 <goldenstar007.dev@gmail.com>
2023-01-20 09:25:04 -0500

    refactor the previous openai API

--- controllers/OpenAIController.js
+++ controllers/OpenAIController.js
@@ -7,6 +7,21 @@ class OpenAIController {
     this.openaiService = new OpenAIService();
   }
 
+  async copyWebsite(req, res, next) {
+    try {
+      const response = await this.openaiService.copyWebsite(req.body);
+
+      const statusCode = response.status;
+      const result = response.data;
+
+      return res.status(statusCode).json(result);
+    } catch (err) {
+      return res
+        .status(err.response?.status || 500)
+        .json({ result: err.response?.statusText || "SERVER ERROR" });
+    }
+  }
+
   async createWebsite(req, res, next) {
     try {
       const response = await this.openaiService.createWebsite(req.body);
@@ -15,8 +30,10 @@ class OpenAIController {
       const result = response.data.choices[0].text;
 
       return res.status(statusCode).json({ result });
-    } catch (error) {
-      return next(error);
+    } catch (err) {
+      return res
+        .status(err.response?.status || 500)
+        .json({ result: err.response?.statusText || "SERVER ERROR" });
     }
   }
 }
